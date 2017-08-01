d034316e7f57e828185c58552352c92dac1332a9
杨晓鹏 <bjyangxiaopeng1@corp.netease.com>
2017-08-01 15:54:20 +0800

    [0.5.1]对Dispatch中的vecMonitors,进行加锁控制.防止多线程访问时,出现崩溃问题

--- Bigger/core/BLogger.cc
+++ Bigger/core/BLogger.cc
@@ -9,29 +9,35 @@
 #include "BLogger.h"
 
 #include <vector>
+#include <mutex>
 using namespace std;
 
 vector<BLogMonitor *> vecMonitors;
-
-
+mutex mutex4Monitor;
 
 void BLogDispatcher::WriteLog(BLogType eLogType, const char *pLog) {
+    mutex4Monitor.lock();
     vector<BLogMonitor *>::iterator it;
     for (it=vecMonitors.begin(); it!=vecMonitors.end(); it++) {
         if ((*it)->_MonitorType&eLogType) {
             (*it)->Callback(eLogType, pLog);
         }
     }
+    mutex4Monitor.unlock();
     
     return;
 }
 
 int BLogDispatcher::RegisterMonitor(BLogMonitor *pMonitor) {
+    mutex4Monitor.lock();
     vecMonitors.push_back(pMonitor);
+    mutex4Monitor.unlock();
+    
     return 1;
 }
 
 int BLogDispatcher::DeReisterMonitor(BLogMonitor *pMonitor) {
+    mutex4Monitor.lock();
     vector<BLogMonitor *>::const_iterator it;
     for (it=vecMonitors.begin(); it!=vecMonitors.end();) {
         if ((*it)->_MonitorID == pMonitor->_MonitorID) {
@@ -41,6 +47,7 @@ int BLogDispatcher::DeReisterMonitor(BLogMonitor *pMonitor) {
             it++;
         }
     }
+    mutex4Monitor.unlock();
     
     return 1;
 }
